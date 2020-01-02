59e0a00a2c6e12fbe46075749bc86884e25c7be3
Charlie Deets <charlie.deets@gmail.com>
2020-01-02 09:22:13 -0800

    Initial Update

---
+++ Count Artboards.sketchplugin/Contents/Sketch/script.js
@@ -0,0 +1,15 @@
+          var onRun = function(context) {
+          var sketch = require('sketch');
+var UI = require('sketch/ui');
+const documentData = sketch.getSelectedDocument().sketchObject.documentData();
+if (documentData.allArtboards().count() > 1) {
+    UI.message("There are " + documentData.allArtboards().count() + " artboards in this document.");
+} else if (documentData.allArtboards().count() == 1) {
+    UI.message("You have one artboard in this document.");
+} else if (documentData.allArtboards().count() == 0) {
+    UI.message("You do not yet have any artboards in this document.");
+} else {
+    UI.message("Something unsual has happened.");
+}
+          };
+          
